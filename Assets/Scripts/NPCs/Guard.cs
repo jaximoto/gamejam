29ad96b11b8a918a12481b3e8fd86dbb44e37ffa
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-02-25 21:05:21 -0800

    Creating rudimentary line of sight fov

---
+++ Assets/Scripts/NPCs/Guard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : NPC
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.setupFov();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/NPCs/NPC.cs
+++ Assets/Scripts/NPCs/NPC.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+public abstract class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Mesh mesh;
+    Vector3[] vertices;
+    Vector2[] uv;
+    int[] triangles;
+    public virtual void Move()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void setupFov()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        vertices = new Vector3[3];
+        uv = new Vector2[3];
+        triangles = new int[3];
+
+        vertices[0] = Vector3.zero;
+        vertices[1] = new Vector3(50, 0);
+        vertices[2] = new Vector3(0, -50);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
     }
+
+    public virtual void updateFov()
+    {
+
+    }
+
 }
